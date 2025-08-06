1464e6bc5f5cc63ce19c4861f9018f12ae5ae147
pstdigital <ensosmed@gmail.com>
2025-08-06 11:17:38 +0700

    Add project files

---
+++ app/Models/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    // Tambahkan ini:
+    public $incrementing = false;       // id bukan auto increment
+    protected $keyType = 'string';      // id bertipe string (UUID)
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    // Tambahkan boot method untuk generate UUID otomatis
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
+    }
+}
